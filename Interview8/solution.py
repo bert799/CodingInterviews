8c58696ad3365a4b221795e6cdb4abbddf414b68
Bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2024-04-16 19:18:24 -0300

    Aprimorando legibilidade do código

--- Interview8/solution.py
+++ Interview8/solution.py
@@ -10,25 +10,18 @@ def compute_pond_sizes(land: list[list[int]]) -> list[int]:
             search_water(land, row+1, 0)
             return
         if land[row][col] == 0:
-            ponds.append(search_adjacent(land, row, col))
+            ponds.append(search_adjacent(row, col))
             land[row][col] = -1
         search_water(land,row,col+1)
     
-    def search_adjacent(land, row, col):
+    def search_adjacent(row, col):
         num_water = 0
-        if row >= max_row or col >= max_col or row < 0 or col < 0:
+        if row >= max_row or col >= max_col or row < 0 or col < 0 or land[row][col] != 0:
             return num_water
         if land[row][col] == 0:
             num_water += 1
             land[row][col] = -1
-            num_water += search_adjacent(land, row, col+1)
-            num_water += search_adjacent(land, row+1, col)
-            num_water += search_adjacent(land, row-1, col)
-            num_water += search_adjacent(land, row, col-1)
-            num_water += search_adjacent(land, row+1, col+1)
-            num_water += search_adjacent(land, row-1, col-1)
-            num_water += search_adjacent(land, row+1, col-1)
-            num_water += search_adjacent(land, row-1, col+1)
+            num_water += sum(search_adjacent(row + r, col + c) for r in [-1, 0, 1] for c in [-1, 0, 1])
         return num_water
         
     search_water(land, 0, 0)
